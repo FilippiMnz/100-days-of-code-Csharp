76aa446c102eeb8b9cfa0294dda9d1c6a3ced843
Filippi Reis <85193619+FilippiMnz@users.noreply.github.com>
2023-01-01 10:53:30 -0300

    FirstDay

---
+++ ConvertAgeToDays/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+ 
+namespace Convert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira sua idade");
+            int AgeUser = int.Parse(Console.ReadLine());
+            int LeapYear = AgeUser / 4;
+            int FinalMath = (AgeUser * 365) + LeapYear;
+            Console.WriteLine("Vossa senhoria já vivificaste um total de: " + FinalMath + " dias");
+            Console.WriteLine("O total de ano bissextos vivenciados por vossa excelencia totaliza: " + LeapYear);
+        }
+    }
+}
